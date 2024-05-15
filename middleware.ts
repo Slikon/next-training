80fe119c428ce8bbeafe447220d1196da2bd08b5
Viacheslav Kulyk <Viacheslav.Kulyk@nordstrom.com>
2024-05-15 13:41:36 +0300

    feat: add mobile navbar and global search

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
+// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up']);
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) auth().protect();
+  console.log(req.url)
+  // if (!isPublicRoute(req)) auth().protect();
 });
 
 
-export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+// export const config = {
+//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
